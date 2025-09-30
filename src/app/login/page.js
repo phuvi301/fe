e194cfcac374b4ab021b08fb097d429cad559141
Vinh <phuvi301@gmail.com>
2025-09-30 15:39:57 +0700

    add some details

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect } from 'react';
 import { useRouter } from "next/navigation";
 import './login.css';
 
--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
     <div>
       <span>Hello, This is the homepage of the application!</span>
       <br/>
-      <a href="/login">Click here to login</a>
+      <span>Click <a href="/login">here</a> to go to the login page.</span>
     </div>
   );
 }
