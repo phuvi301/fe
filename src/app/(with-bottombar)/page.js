c15b9dae7dec3585bc7bb17d9a300628f51081fc
Vuxchan <luchanvu32123@gmail.com>
2025-10-23 17:07:29 +0700

    hiển thị bài hát có view cao nhất

--- src/app/(with-bottombar)/page.js
+++ src/app/(with-bottombar)/page.js
@@ -9,22 +9,24 @@ import { useEffect, useState } from "react";
 export default function Home() {
     const { bottomBarRef } = useBottomBar();
     const [recentTracks, setRecentTracks] = useState([]);
+    const [mostPlayedTracks, setMostPlayedTracks] = useState([]);
 
     const handleTrackPlay = async (trackId) => {
         await bottomBarRef.current.playTrack(trackId);
     };
 
     useEffect (() => {
-        const recentlyAdded = async () => {
+        const homepageDisplay = async () => {
             try {
-                const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/tracks/recent`);
-                setRecentTracks(res.data.data);
-            } catch (err){
+                const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/tracks/display`);
+                setRecentTracks(res.data.recent);
+                setMostPlayedTracks(res.data.mostPlayed)
+            } catch(err) {
                 console.error('Error getting recent tracks', err);
             }
         };
-        recentlyAdded();
-        const interval = setInterval(recentlyAdded, 30000);
+        homepageDisplay();
+        const interval = setInterval(homepageDisplay, 30000);
         return () => clearInterval(interval);
     }, [])
 
@@ -42,7 +44,7 @@ export default function Home() {
             <article className={style["featured-section"]}>
               {/* <h1>More of what you like</h1>
               <p>Recommended for you</p> */}
-              <h1>Recently Added </h1>
+              <h1>Recently added </h1>
               <p>Check out the newest tracks</p>
               <div className={style["featured-container"]}>
                 {recentTracks.map(track => (
@@ -57,33 +59,19 @@ export default function Home() {
             </article>
             {/* Featured container 2 */}
             <article className={style["featured-section"]}>
-              <h1>Trending by genre</h1>
-              <p>Discover what's popular</p>
+              {/* <h1>Trending by genre</h1>
+              <p>Discover what's popular</p> */}
+              <h1>Most Played Tracks</h1>
+              <p>See which songs top the play charts this week.</p>
               <div className={style["featured-container"]}>
-                {/* Song 1 */}
-                <a onClick={() => handleTrackPlay("68f670d2bb66a13123769868")}>
-                  <span><Image src="/song/5.png" width={500} height={500} alt="Album 6" />Treasure</span>
-                </a>
-                {/* Song 2 */}
-                <a onClick={() => handleTrackPlay("68f6766cbb66a13123769875")}>
-                  <span><Image src="/song/6.png" width={500} height={500} alt="Album 7" />Thế Hệ Tan Vỡ</span>
-                </a>
-                {/* Song 3 */}
-                <a onClick={() => handleTrackPlay("68f67a43bb66a13123769884")}>
-                  <span><Image src="/song/7.jpg" width={500} height={500} alt="Album 8" />Quên Dần Quên</span>
-                </a>
-                {/* Song 4 */}
-                <a onClick={() => handleTrackPlay("68f67df6bb66a13123769896")}>
-                  <span><Image src="/song/8.png" width={500} height={500} alt="Album 9" />điều vô tri nhất</span>
-                </a>
-                {/* Song 5 */}
-                <a onClick={() => handleTrackPlay("68f4f53d1c604adcc9499fba")}>
-                  <span><Image src="/song/vicuaanh.png" width={500} height={500} alt="Album 10" unoptimized />Vị của anh</span>
-                </a>
-                {/* Song 6 */}
-                <a onClick={() => handleTrackPlay("68f4fda44e15aeb1eb62f821")}>
-                  <span><Image src="/song/11.jpg" width={500} height={500} alt="Album 11" />Nhạc báo thức brainrot</span>
-                </a>
+                {mostPlayedTracks.map(track => (
+                  <a key={track._id} onClick={() => handleTrackPlay(track._id)}>
+                    <span>
+                      <Image src={track.thumbnailUrl} width={500} height={500} alt={track.title} priority={true} />
+                      {track.title}
+                    </span>
+                  </a>
+                ))}
               </div>
             </article>
           </section>
