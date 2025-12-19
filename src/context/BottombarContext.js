4ca997ce7e37955cdcdbe2f597f042a1a2d659b5
Vinh <phuvi301@gmail.com>
2025-12-19 15:26:51 +0700

    fix recommendation when reload

--- src/context/BottombarContext.js
+++ src/context/BottombarContext.js
@@ -50,10 +50,19 @@ export function BottomBarProvider({ children }) {
     }
 
     // Đề xuất playlist dựa trên bài hát hiện tại
-    const recommendPlaylist = async (songID) => {
+    const recommendPlaylist = async (songID, refresh = true) => {
         try {
-            // Gọi API đề xuất
-            const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/tracks/recommend/${songID}`);
+            const raw = localStorage.getItem("userInfo");
+            if (!raw) return [];
+            const parsed = JSON.parse(raw); 
+            const accessToken = parsed?.accessToken || null;
+
+            const res = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/tracks/recommend/${songID}?refresh=${refresh}`, {
+                headers: {
+                    token: `Bearer ${accessToken}`
+                },
+                withCredentials: true,
+            });
             return res.data.data || [];
         } catch (err) {
             console.error("Can't get recommended playlist", err);
@@ -122,7 +131,7 @@ export function BottomBarProvider({ children }) {
             else if (playlistID.startsWith("recommend-")) {
                 // Playlist đề xuất ảo
                 if (!tracks) {
-                    tracks = await recommendPlaylist(nowPlaying.current._id);                
+                    tracks = await recommendPlaylist(nowPlaying.current._id, false); // Lấy danh sách đề xuất đã lưu
                 }
                 setPlaylistPlaying({
                     _id: playlistID,
