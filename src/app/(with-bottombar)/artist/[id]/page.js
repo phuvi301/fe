1918810f2149b4e3576f9a96386e9031649ff26d
Vinh <phuvi301@gmail.com>
2025-12-20 17:03:29 +0700

    fix linh tinh follow

--- src/app/(with-bottombar)/artist/[id]/page.js
+++ src/app/(with-bottombar)/artist/[id]/page.js
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { use, useEffect, useState } from 'react';
 import { useParams } from 'next/navigation';
 import axios from 'axios';
 import Image from 'next/image';
@@ -69,6 +69,7 @@ export default function ArtistPage() {
             const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/follow/${id}/follow-status`, {
                 headers: { token: `Bearer ${token}` }
             });
+
             setIsFollowing(response.data.data.isFollowing);
         } catch (error) {
             console.error("Error checking follow status:", error.response?.status, error.response?.data || error.message);
@@ -85,6 +86,8 @@ export default function ArtistPage() {
                 return;
             }
             
+            const userInfo = localStorage.getItem("userInfo");
+
             if (isFollowing) {
                 // Unfollow
                 await axios.delete(`${process.env.NEXT_PUBLIC_API_URL}/api/follow/${id}/follow`, {
@@ -95,6 +98,12 @@ export default function ArtistPage() {
                 setArtistData(prev => ({
                     ...prev,
                     followerCount: Math.max((prev.followerCount || 0) - 1, 0)
+                }));
+
+                // Giảm followingCount trong localStorage
+                userInfo && localStorage.setItem("userInfo", JSON.stringify({
+                    ...JSON.parse(userInfo),
+                    followingCount: Math.max((JSON.parse(userInfo).followingCount || 0) - 1, 0)
                 }));
 
                 setTimeout(async () => {
@@ -119,6 +128,12 @@ export default function ArtistPage() {
                 setArtistData(prev => ({
                     ...prev,
                     followerCount: (prev.followerCount || 0) + 1
+                }));
+
+                // Tăng followingCount trong localStorage
+                userInfo && localStorage.setItem("userInfo", JSON.stringify({
+                    ...JSON.parse(userInfo),
+                    followingCount: (JSON.parse(userInfo).followingCount || 0) + 1
                 }));
 
                 setTimeout(async () => {
